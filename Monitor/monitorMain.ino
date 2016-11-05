b52a021dd0d622d5f861e6b869f950e75405dc8f
Sérgio Dias <sergio.dias@gemini-solutions.net>
2016-11-04 23:18:23 -0300

    Primeira versão do código para o Monitor (Incompleta)

---
+++ Monitor/monitorMain.ino
@@ -0,0 +1,95 @@
+#include <ESP8266WiFi.h>
+#include <HX711.h>
+#include <Time.h>
+
+// Wifi Parâmetros & Módulo ESP8266
+const char* ssid = "service-set-identifier";  // SSID da rede Wifi
+const char* password = "network-password";    // Senha da rede Wifi
+
+// Detecção de Presença & Sensor de Infravermelho
+int extraLedPin = 8;    // Pino onde o LED extra de feedback está conectado
+int infraPin = 7;       // Pino onde a saída do sensor óptico está conectado
+int detectedObject = 1; // Estado de presença de objeto (0 - detectado, 1 - não detectado)
+
+// Sensor de Peso & Leitura da Carga
+const int dOut = A3;    // Pino DT
+const int hxSCK = A4;   // Pino SCK
+float objWeight = 0;
+
+// Cache de Leituras de objetos
+int notSent = 0;        // Contador de msgs não enviadas à central sobre pacotes monitorados 
+time_t latestObj = -1;  // Timestamp do último objeto monitorado
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // Inicialização do sensor de infravermelho e LED extra
+  pinMode(extraLedPin, OUTPUT);   // Definição do pino de LED como saída
+  pinMode(infraPin, INPUT);       // Definição do pino do sensor como entrada
+
+  // Inicialização do sensor de peso
+  pinMode(hxSCK, OUTPUT);
+  pinMode(dOut, INPUT);
+  digitalWrite(hxSCK, HIGH);      // Reset do módulo HX711
+  delay(200);
+  digitalWrite(hxSCK, LOW);
+}
+
+// Leitura do peso
+void readWeight(){ 
+  unsigned long weight = 0;
+  
+  digitalWrite(hxSCK, LOW);
+  
+  while (digitalRead(dOut) == 1) ;
+  
+  for (int i=0; i < 24; i++){
+    digitalWrite(hxSCK, HIGH);
+    weight = weight << 1;
+    digitalWrite(hxSCK, LOW);
+    
+    if(digitalRead(dOut) == 1) weight++;
+  }
+  
+  digitalWrite(hxSCK, HIGH);
+  digitalWrite(hxSCK, LOW);
+  
+  weight ^= 0x00800000;
+  objWeight = weight;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(extraLedPin, LOW);         // Apaga Led extra de presença de objeto
+  objWeight = 0;                          // Limpa valor de leitura de peso para objeto
+
+  // Verifica Sensor Presença
+  detectedObject = digitelRead(infraPin); // Atualiza estado de presença de objeto
+  // Se dectectado um objeto
+  if(detectedObject == LOW) {
+    // Acende Led extra de presença de objeto
+    digitalWrite(extraLedPin, HIGH);      
+    
+    // Atualiza última ocorrência de objeto monitorado
+    latestObj = now();
+    
+    // Verifica e Salva Peso
+    readWeight();
+
+    notSent++;
+  }
+
+  // Se conectado ao WiFi
+  if(Wifi.status() == WL_CONNECTED) {
+      // Cria um cliente TCP
+      // Conecta-se ao cliente TCP
+      // Se conectado com sucesso
+        // Envia msg com informações do pacote à central
+
+        // Verifica/Aguarda confirmação
+          // Confirmação recebida
+          notSent--;
+  }
+   
+  delay(500);
+}
